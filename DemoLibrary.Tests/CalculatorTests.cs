ec0fd66212570d7ad709105f6336e91de5d75d65
YL <simon-ling@hotmail.com>
2021-09-28 20:05:40 -0400

    done

---
+++ DemoLibrary.Tests/CalculatorTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoLibrary;
+using Xunit;
+
+namespace DemoLibrary.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_SimpleValueShouldCalculate()
+        {
+            //arrange
+            double expected = 5;
+
+            //act
+            double actual = Calculator.Add(3, 2);
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
