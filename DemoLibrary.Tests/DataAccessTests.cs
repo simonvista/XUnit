3cb5069306831b8feae3c6df78dda44de9d37b62
YL <simon-ling@hotmail.com>
2021-09-28 22:19:52 -0400

    done

---
+++ DemoLibrary.Tests/DataAccessTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using DemoLibrary;
+using DemoLibrary.Models;
+
+namespace DemoLibrary.Tests
+{
+    public class DataAccessTests
+    {
+        [Fact]
+        public void AddPersonToPeopleList_ShouldWork()
+        {
+            PersonModel newPerson = new PersonModel
+            {
+                FirstName = "John",
+                LastName = "Doe"
+            };
+            List<PersonModel> person = new List<PersonModel>();
+            DataAccess.AddPersonToPeopleList(person, newPerson);
+            Assert.True(person.Count == 1);
+            Assert.Contains<PersonModel>(newPerson, person);
+        }
+    }
+}
