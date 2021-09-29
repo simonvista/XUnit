ec7c45caa995dcfc8a78311d6cebf025eee9f88a
YL <simon-ling@hotmail.com>
2021-09-28 22:43:16 -0400

    done

--- DemoLibrary.Tests/DataAccessTests.cs
+++ DemoLibrary.Tests/DataAccessTests.cs
@@ -24,5 +24,20 @@ namespace DemoLibrary.Tests
             Assert.True(person.Count == 1);
             Assert.Contains<PersonModel>(newPerson, person);
         }
+        [Theory]
+        [InlineData("Tim", "", "LastName")]
+        [InlineData("","Corey","FirstName")]
+        public void AddPersonToPeopleList_ShouldThrowError(string firstName, string lastName, string param)
+        {
+            PersonModel newPerson = new PersonModel
+            {
+                FirstName = firstName,
+                LastName = lastName
+            };
+            List<PersonModel> person = new List<PersonModel>();
+            //DataAccess.AddPersonToPeopleList(person, newPerson);
+            Assert.Throws<ArgumentException>(param, () => DataAccess.AddPersonToPeopleList(person, newPerson));
+            Assert.Throws<ArgumentException>(param, () => DataAccess.AddPersonToPeopleList(person, newPerson));
+        }
     }
 }
