4e1d0187a2c8d0dab09cd9e3ae1de66514bac6ef
YL <simon-ling@hotmail.com>
2021-09-28 21:28:30 -0400

    done

---
+++ DemoLibrary.Tests/ExamplesTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using DemoLibrary;
+
+namespace DemoLibrary.Tests
+{
+    public class ExamplesTests
+    {
+        [Fact]
+        public void ExampleLoadTextFile_ValidNameShouldWork()
+        {
+            string actual = Examples.ExampleLoadTextFile("this is a valid file name");
+            Assert.True(actual.Length > 0);
+        }
+        [Fact]
+        public void ExampleLoadTextFile_InvalidNameShouldFail()
+        {
+            Assert.Throws<System.IO.FileNotFoundException>(()=>Examples.ExampleLoadTextFile("this is a"));
+        }
+    }
+}
